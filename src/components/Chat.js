cf0ec8db670a7c843b26e2be51b797ab30e01849
nahuel-retamoso <nahuel.retamoso@gmail.com>
2023-06-28 21:08:43 -0300

    button color

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -42,7 +42,7 @@ const Chat = ({ send, set, message, messagesList }) => {
       <InputGroup color='whiteAlpha.900' boxShadow="0px 8px 32px rgba(31, 38, 135, 0.37)" bg="rgba(255, 255, 255, 0.1)" overflow={'hidden'} borderRadius='lg'>
         <Input value={message} border={'InactiveBorder'} onChange={(event) => set(event.target.value)} onKeyDown={handleKeyPress} />
         <InputRightAddon p='0px' >
-          <Button borderRadius={'none'} variant={'ghost'} color='whiteAlpha.900' onClick={() => send()}>Send</Button>
+          <Button borderRadius={'none'} variant={'ghost'} colorScheme='whiteAlpha.900' onClick={() => send()}>Send</Button>
         </InputRightAddon>
       </InputGroup>
     </Flex>
